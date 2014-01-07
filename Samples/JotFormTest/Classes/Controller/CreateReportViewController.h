2b97420c6d39592699dd6fdff12676bc025c108c
Wang Yu Ping <wang@jotform.com>
2014-01-08 00:40:02 +0800

    Added UI for creating report
    Added UI for creating report

---
+++ Samples/JotFormTest/Classes/Controller/CreateReportViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateReportViewController.h
+//  JotFormTest
+//
+//  Created by Administrator on 1/8/14.
+//  Copyright (c) 2014 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateReportViewController : UIViewController
+
+@end
---
+++ Samples/JotFormTest/Classes/Controller/CreateReportViewController.m
@@ -0,0 +1,38 @@
+//
+//  CreateReportViewController.m
+//  JotFormTest
+//
+//  Created by Administrator on 1/8/14.
+//  Copyright (c) 2014 wang. All rights reserved.
+//
+
+#import "CreateReportViewController.h"
+
+@interface CreateReportViewController ()
+
+@end
+
+@implementation CreateReportViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
