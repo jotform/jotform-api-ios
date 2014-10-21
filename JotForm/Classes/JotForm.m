6918e165c20cbd2d6fb8a339b4f74a3f8c96d2d6
Curtis Stilwell <stilcurt@yahoo.com>
2014-10-21 15:25:46 -0500

    Moved debug for parameter in a more appropriate spot.

--- JotForm/Classes/JotForm.m
+++ JotForm/Classes/JotForm.m
@@ -78,9 +78,9 @@
     NSString *paramstr = [paramarray componentsJoinedByString:@"&"];
     urlStr = [NSString
         stringWithFormat:@"%@?apikey=%@&%@", urlStr, apiKey, paramstr];
-
-    [self debugLog:[NSString stringWithFormat:@"paramstr = %@", paramstr]];
   }
+  
+  [self debugLog:[NSString stringWithFormat:@"paramstr = %@", paramstr]];
 
   if ([method isEqualToString:HTTPREQUEST_METHOD_DELETE] == YES ||
       [method isEqualToString:HTTPREQUEST_METHOD_POST] == YES) {
