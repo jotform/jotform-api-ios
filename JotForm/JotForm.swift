2187fc242e59423a0db56fa9987f0df810e81a86
Curtis-Stilwell <curtisstilwell1@curtiss-macbook-pro.local>
2018-01-21 15:15:18 -0600

    Created JotForm API methods in swift for logging in, logging out and creating reports.

--- JotForm/JotForm.swift
+++ JotForm/JotForm.swift
@@ -41,6 +41,54 @@ class JotForm {
             failureBlock(error as Error)
         })
     }
+    
+    func createReport(_ formID: Int64, reportParams: [AnyHashable: Any], onSuccess successBlock: @escaping (_ id : AnyObject) -> Void, onFailure failureBlock: @escaping (_: Error) -> Void) {
+        let urlStr = "https://submit.jotform.com/submit/\(formID)/"
+        debugLog(urlStr, params: reportParams)
+     
+        manager?.responseSerializer = AFHTTPResponseSerializer()
+        
+        manager?.post(urlStr, parameters: reportParams, progress: nil, success: {(_ task: URLSessionTask, _ responseObject: Any) -> Void in
+            successBlock(responseObject as AnyObject)
+        }, failure: {(_ operation: URLSessionDataTask?, _ error: Error) -> Void in
+            failureBlock(error as Error)
+        })
+    }
+    
+    func createSuggestion(_ formID: Int64, suggestionParams: [AnyHashable: Any], onSuccess successBlock: @escaping (_ id: AnyObject) -> Void, onFailure failureBlock: @escaping (_: Error) -> Void) {
+        let urlStr = "https://submit.jotform.me/submit/\(formID)/"
+        debugLog(urlStr, params: suggestionParams)
+       
+        manager?.responseSerializer = AFHTTPResponseSerializer()
+        
+        manager?.post(urlStr, parameters: suggestionParams, progress: nil, success: {(_ task: URLSessionTask, _ responseObject: Any) -> Void in
+            successBlock(responseObject as AnyObject)
+        }, failure: {(_ operation: URLSessionDataTask?, _ error: Error) -> Void in
+            failureBlock(error as Error)
+        })
+    }
+    
+    func login(_ userinfo: [AnyHashable: Any], onSuccess successBlock: @escaping (_ id: AnyObject) -> Void, onFailure failureBlock: @escaping (_: Error) -> Void) {
+        let urlStr = "\(baseUrl)/user/login"
+        debugLog(urlStr, params: userinfo)
+       
+        manager?.post(urlStr, parameters: userinfo, progress: nil, success: {(_ task: URLSessionTask, _ responseObject: Any) -> Void in
+            successBlock(responseObject as AnyObject)
+        }, failure: {(_ operation: URLSessionDataTask?, _ error: Error) -> Void in
+            failureBlock(error as Error)
+        })
+    }
+    
+    func logout(_ userinfo: [AnyHashable: Any], onSuccess successBlock: @escaping (_ id: AnyObject) -> Void, onFailure failureBlock: @escaping (_: Error) -> Void) {
+        let urlStr = "\(baseUrl)/user/logout?apiKey=\(apiKey)"
+        debugLog(urlStr, params: userinfo)
+        manager?.post(urlStr, parameters: userinfo, progress: nil, success: {(_ task: URLSessionTask, _ responseObject: Any) -> Void in
+            successBlock(responseObject as AnyObject)
+        }, failure: {(_ operation: URLSessionDataTask?, _ error: Error) -> Void in
+            failureBlock(error as Error)
+        })
+    }
+
 }
 
 
