4028a14e90f7a8c8e884cc77ade7aadf95a0b14f
Curtis-Stilwell <curtisstilwell1@curtiss-macbook-pro.local>
2018-01-28 12:21:27 -0600

    Fixed pod warnings.

--- Pods/Target Support Files/AFNetworking-2b1e3ee3/AFNetworking-2b1e3ee3-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AFNetworking_2b1e3ee3 : NSObject
-@end
-@implementation PodsDummy_AFNetworking_2b1e3ee3
-@end
--- Pods/Target Support Files/AFNetworking-b0a86b24/AFNetworking-b0a86b24-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AFNetworking_b0a86b24 : NSObject
-@end
-@implementation PodsDummy_AFNetworking_b0a86b24
-@end
